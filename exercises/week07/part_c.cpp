d2c717d335e723b7e1c6a8a42c26fd96f35f7f69
Jon-Magnus Rosenblad <jonmagnusr@gmail.com>
2018-11-19 13:27:06 +0100

    Trimmed extra code from part_c.cpp

---
+++ exercises/week07/part_c.cpp
@@ -0,0 +1,41 @@
+#include <cstdio> 
+#include <cmath> 
+#include <map>
+using namespace std;
+
+class Polynomial
+{
+private:
+	map<int,double> coeff;
+public:
+	Polynomial(map<int,double> &coeff_) : coeff(coeff_) {}
+
+	double operator() (double x)
+	{
+		double sum = 0;
+		for (map<int,double>::const_iterator it = coeff.begin(); it != coeff.end(); it++)
+			sum += it->second*pow(x,it->first);
+		return sum;
+	}
+
+	void print()
+	{
+		for (map<int,double>::const_iterator it = coeff.begin(); it != coeff.end(); it++)
+			printf("%s%lf*x^%d", it == coeff.begin() ? "" : " + ", it->second, it->first);
+		printf("\n");
+	}
+};
+
+int main()
+{
+	map<int,double> coeff;
+	coeff[10] = 1, coeff[5] = 5, coeff[0] = 1;
+	Polynomial p(coeff);
+
+	p.print();
+
+	printf("p(%lf)=%lf\n",(double)0,p(0));
+	printf("p(%lf)=%lf\n",(double)1,p(1));
+
+	return 0;
+}
